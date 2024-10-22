d8b2f4c1333ba458d5a14cdd0ab0842c7d5e44d1
lupend516 <cryptopend@bk.ru>
2024-10-22 17:40:50 +0500

    Create sdp w1.java

---
+++ sdp w1.java
@@ -0,0 +1,19 @@
+public class DataModel {
+    private String data;
+
+    public DataModel() {
+        this.data = "Initial Data";
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void updateData(String newData) {
+        this.data = newData;
+    }
+
+    public void combineData(String moreData) {
+        this.data += " | " + moreData;
+    }
+}
