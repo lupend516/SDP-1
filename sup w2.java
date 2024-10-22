8e05b55bea790e95432f9dbfddf2811e55666768
lupend516 <cryptopend@bk.ru>
2024-10-22 17:43:17 +0500

    Create sup w2.java

---
+++ sup w2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ConsoleView {
+    private Scanner scanner;
+
+    public ConsoleView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void displayData(String data) {
+        System.out.println("Current Data: " + data);
+    }
+
+    public String gatherInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
